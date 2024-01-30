00ad0c850d2e1b5c7102d9000359c39011322c4e
周竑亦 <chou960916@gmail.com>
2024-01-30 19:05:18 +0800

    initial commit, Swerve Complete.

--- src/main/java/frc/robot/Constants/RobotMap.java
+++ src/main/java/frc/robot/Constants/RobotMap.java
@@ -5,18 +5,18 @@ public class RobotMap {
     
     // TO DO : Add CAN IDs
 
-    public final static int FRONT_LEFT_DRIVE_MOTOR_ID = 0;
-    public final static int BACK_LEFT_DRIVE_MOTOR_ID = 0;
-    public final static int BACK_RIGHT_DRIVE_MOTOR_ID = 0;
-    public final static int FRONT_RIGHT_DRIVE_MOTOR_ID = 0;
+    public final static int FRONT_LEFT_DRIVE_MOTOR_ID = 1;
+    public final static int BACK_LEFT_DRIVE_MOTOR_ID = 2;
+    public final static int BACK_RIGHT_DRIVE_MOTOR_ID = 3;
+    public final static int FRONT_RIGHT_DRIVE_MOTOR_ID = 4;
 
-    public final static int FRONT_LEFT_ANGLE_MOTOR_ID = 0;
-    public final static int BACK_LEFT_ANGLE_MOTOR_ID = 0;
-    public final static int BACK_RIGHT_ANGLE_MOTOR_ID = 0;
-    public final static int FRONT_RIGHT_ANGLE_MOTOR_ID = 0;
+    public final static int FRONT_LEFT_ANGLE_MOTOR_ID = 1;
+    public final static int BACK_LEFT_ANGLE_MOTOR_ID = 2;
+    public final static int BACK_RIGHT_ANGLE_MOTOR_ID = 3;
+    public final static int FRONT_RIGHT_ANGLE_MOTOR_ID = 4;
     
-    public final static int FRONT_LEFT_CANCODER_MOTOR_ID = 0;
-    public final static int BACK_LEFT_CANCODER_MOTOR_ID = 0;
-    public final static int BACK_RIGHT_CANCODER_MOTOR_ID = 0;
-    public final static int FRONT_RIGHT_CANCODER_MOTOR_ID = 0;
+    public final static int FRONT_LEFT_CANCODER_MOTOR_ID = 1;
+    public final static int BACK_LEFT_CANCODER_MOTOR_ID = 2;
+    public final static int BACK_RIGHT_CANCODER_MOTOR_ID = 3;
+    public final static int FRONT_RIGHT_CANCODER_MOTOR_ID = 4;
 }
