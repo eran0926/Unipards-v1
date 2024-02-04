78a6b2d8c31c6d0b086aee9cd9b803ecb8d61aa0
周竑亦 <chou960916@gmail.com>
2024-02-04 16:20:38 +0800

    Update ShootSubSystem.java

--- src/main/java/frc/robot/SubSystem/ShootSubSystem.java
+++ src/main/java/frc/robot/SubSystem/ShootSubSystem.java
@@ -1,11 +1,16 @@
 package frc.robot.SubSystem;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RobotMap;
 
 public class ShootSubSystem extends SubsystemBase {
     private CANSparkMax mShootMotor;
+    private RelativeEncoder mShootEncoder;
     public ShootSubSystem() {
+        mShootMotor = new CANSparkMax(RobotMap.SHOOT_MOTOR_ID, CANSparkMax.MotorType.kBrushless);
+        mShootEncoder = mShootMotor.getEncoder();
     }
 
     public void shoot() {
